c86db91886b5e7d24f99a3c08141986795afea73
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-12-10 16:55:59 -0500

    home page styling

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,7 +1,12 @@
+import Navbar from "../Navbar/Navbar";
+import "../Styling/Home.css"
+// import Temp from "../Photos/Temp-home-photo.jpeg";
 function Home(){
     return(
         <div className="Home">
-            <h1>Test</h1>
+            <mark>Megan Proctor</mark>
+            <Navbar />
+            {/* <img src={Temp} alt="home" className="Cover"/> */}
         </div>
     )
 }
