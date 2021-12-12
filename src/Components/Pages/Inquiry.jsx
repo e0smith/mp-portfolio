914fe82b571a4ae5f35af9fd0bce39ce455fcf6b
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-12-12 16:29:46 -0500

    inquiry component styling modified

--- src/Components/Pages/Inquiry.jsx
+++ src/Components/Pages/Inquiry.jsx
@@ -1,61 +1,73 @@
-import { Container, NameMark } from "../Container.style"
-import { SubBodyForm, TextArea, InputText, SubmitButton, FormBox } from "../Container.style"
+import {
+    SubBodyForm,
+    TextArea,
+    InputText,
+    SubmitButton,
+    FormBox,
+    Container,
+    NameMark,
+    InfoHeader,
+    InfoBox,
+    H2Boarder
+} from "../Container.style"
+
 export default function About(){
 
     return(
         <Container>
             <NameMark>Megan Proctor</NameMark>
-            <FormBox>
-                <header> Inquiry </header>
-                <SubBodyForm>
-                    <h2>Inquiry</h2>
-                    <div className="info-box">
-                        <div>
-                            <p>Please fill out the following "Inquiry form" with necessary information and inquiry or comments, and press "submit".</p>
-                        </div>
-                        <div className="inquiry-form">
-                            <form method="post">
-                                <table width="100%" summary="Inquiry form">
-                                    <tbody>
-                                        <tr>
-                                            <th scope="row" className="form">
-                                                Company name, Gallery name
-                                            </th>
-                                                <InputText type="text" name="company"/>
-                                                <input type="hidden" name="company" value="company"/>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row" className="form" >
-                                                Name
-                                            </th>
-                                                <InputText type="text" name="name"/>
-                                                <input type="hidden" name="name" value="name"/>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row" className="form" >
-                                                E-mail
-                                            </th>
-                                                <InputText type="text" name="email"/>
-                                                <input type="hidden" name="email" value="email"/>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row" className="form" >
-                                                Message
-                                            </th>
-                                                <TextArea rows="10" name="text"></TextArea>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                                <div className="form-btn-box">
-                                    <div className="form-btn-submit">
-                                        <SubmitButton type="submit" value="submit"/>
+            <InfoBox>
+                {/* <FormBox> */}
+                    <InfoHeader> Inquiry </InfoHeader>
+                    <SubBodyForm>
+                        <div className="info-box">
+                            <div>
+                                <p>Please fill out the following "Inquiry form" with necessary information and inquiry or comments, and press "submit".</p>
+                            </div>
+                            <div className="inquiry-form">
+                                <form method="post">
+                                    <table width="100%" summary="Inquiry form">
+                                        <tbody>
+                                            <tr>
+                                                <th scope="row" className="form">
+                                                    Company Name
+                                                </th>
+                                                    <InputText type="text" name="company"/>
+                                                    <input type="hidden" name="company" value="company"/>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row" className="form" >
+                                                    Name
+                                                </th>
+                                                    <InputText type="text" name="name"/>
+                                                    <input type="hidden" name="name" value="name"/>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row" className="form" >
+                                                    E-mail
+                                                </th>
+                                                    <InputText type="text" name="email"/>
+                                                    <input type="hidden" name="email" value="email"/>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row" className="form" >
+                                                    Message
+                                                </th>
+                                                    <TextArea rows="10" name="text"></TextArea>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <div className="form-btn-box">
+                                        <div className="form-btn-submit">
+                                            <SubmitButton type="submit" value="submit">Submit</SubmitButton>
+                                        </div>
                                     </div>
-                                </div>
-                            </form>
+                                </form>
+                            </div>
                         </div>
-                    </div>
-                </SubBodyForm>
-            </FormBox>
+                    </SubBodyForm>
+                {/* </FormBox> */}
+            </InfoBox>
         </Container>
     )
 }
