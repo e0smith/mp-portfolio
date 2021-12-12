53ba276faf154641dfbc3325a7cd1766ef32e949
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-12-12 16:29:17 -0500

    policy component written

---
+++ src/Components/Pages/Policy.jsx
@@ -0,0 +1,49 @@
+import { NameMark, Container, InfoBox } from "../Container.style";
+export default function Policy(){
+
+    return(
+        <Container>
+            <NameMark><strong>Megan </strong>Proctor</NameMark>
+            <div>
+                <h2> I. Proprietary Rights </h2>
+                <br/>
+                <p> The photographs contained on this, and any other site under the proctorphotos.com domain, are the property of Megan V. Proctor and are protected by United States and International copyright laws. All copyright, trademark, and other intellectual property rights in this site, are the property of Megan V. Proctor. </p>
+                <br/>
+                <h2> All Photographs on proctorphotos.com are copyrighted. </h2>
+                <br/>
+                <p> No permission, either express or implied, is granted for the electronic transmission, storage, retrieval, or printing of the photographs contained on proctorphotos.com, other than that which is related to the normal "caching" practices of web browsers and activities such as downloading solely for your personal enjoyment. No parties/individuals visiting or viewing images from proctorphotos.com, may otherwise copy, modify, publish, transmit, or distribute the contents of the photographs found herein.
+                     Express written permission must be granted, on behalf of the photographer (copyright holder, Megan V. Proctor), in order to use these photographs for any purpose that is not outlined above.</p>
+                <p><strong>Your use of proctorphotos.com or viewing of this statement indicates your acceptance of these conditions. </strong></p>
+                <br/>
+                <h2> II. Personal Use Criteria </h2>
+                <br/>
+                <p>The following are a list of conditions where I, Megan V. Proctor, give individuals permission to link to (or copy) JPEG images and other material at no charge: </p>
+                <ol>
+                    <li> You may print a copy of any photograph for personal reference without  further permission. </li>
+                    <li> Students are welcome to download images and incorporate them into school reports, presentations, etc. for free so long as proper credit/citation is given, without my further permission. </li>
+                    <li> Anyone can download images from Desktops / Wallpapers for the express use of displaying them on screen on any personal computer that is not being used for commercial or public viewing purposes. </li>
+                </ol>
+                <br/>
+                <h2> III. Free Web Usage Criteria </h2>
+                <p> The following conditions define my authorization for use of my image on the Web, free of charge, so long as proper attribution of copyright is given to me, Megan V. Proctor in said attribution: </p>
+                <ol>
+                    <li> You may make LINKS to the images on pages you maintain for yourself for personal, non-commercial use. </li>
+                    <li> If you are preparing FREE online site guides for other photographers, bloggers, and photograph forums, you may make use of these images, BUT ONLY IF YOU ATTRIBUTE THEM AS DESCRIBED ABOVE. </li>
+                </ol>
+                <h2> IV. Commercial Use </h2>
+                <p> Those persons who wish to use the photography found on this site in a commercial fashion must receive written consent from me, Megan V. Proctor, prior to its commercial use. To obtain consent, interested parties should send an e-mail to (insert). In that e-mail the following should be addressed:</p>
+                <ol>
+                    <li> Image(s) you would like to use </li>
+                    <li> Where they will be displayed </li>
+                    <li> How they will be displayed </li>
+                    <li> Intent of its display </li>
+                    <li> Duration of their public viewing </li>
+                    <li> Any other information you feel would be useful </li>
+                </ol>
+                <p> Any commercial use of my photography without prior consent is a violation of United States and International copyright law. </p>
+                <h2> V. Copyright Infringement is Serious </h2>
+                <p> The act of infringing on a copyright is a very serious business and is punishable by fines and/or imprisonment. I have worked very hard to present you with a quality photographic experience and I kindly ask that you will refrain from copying anything from the site onto your own hard drive, diskette(s), printer, etc. You are welcome to copy the hyperlinks to other sites found on proctorphotos.com, however, I ask that you do no copy any of the graphics, photographs, or articles from this site for your own use (other than the terms stated above). Doing so is an infringement of United States and International copyright laws. </p>
+            </div>
+        </Container>
+    )
+}
