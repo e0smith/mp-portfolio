74c4d44174758a3afe6adc59fe379fca014d8208
Edward Smith Silvia <esmithsilvia@gmail.com>
2022-01-05 23:15:18 -0500

    generated files

--- mp-backend/server.js
+++ mp-backend/server.js
@@ -2,25 +2,31 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
-const express = require('express')
-const app = express()
-const expressLayouts = require('express-ejs-layouts')
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const cors = require('cors');
+const indexRouter = require('./routes/index');
+const portfoliosRouter = require('./routes/portfolios')
 
-const indexRouter = require('./routes/index')
-
-
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
-
-const mongoose = require('mongoose')
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+app.use(cors());
+// mongoose setup
+const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true})
+    useNewUrlParser: true});
 const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose'));
 
-app.use('/', indexRouter)
-app.listen(process.env.PORT || 3000)
+app.use('/', indexRouter);
+app.use('/portfolios', portfoliosRouter);
+
+
+app.listen(process.env.PORT || 9000);
+
+module.exports = app;
