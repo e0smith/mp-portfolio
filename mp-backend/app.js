1b1a69ca559e73c02fb64fb75aa433923d12bc51
Edward Smith Silvia <esmithsilvia@gmail.com>
2022-02-15 15:29:24 -0500

    backend node package and app.js file created

---
+++ mp-backend/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const port = process.env.PORT || 3080;
+
+// Templating engine
+app.engine('hbs', exphbs[{ extname: '.hbs'}]);
+app.set('view engine', 'hbs');
+
+app.listen(port, ()=> console.log(`Listening on port ${port}`))
+
