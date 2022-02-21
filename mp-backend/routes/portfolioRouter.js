64549928698b365ff1951e34a0f8f327b9d3c39a
Edward Smith Silvia <esmithsilvia@gmail.com>
2022-02-20 21:56:34 -0500

    portfolio router

---
+++ mp-backend/routes/portfolioRouter.js
@@ -0,0 +1,12 @@
+const portfolioController = require('../controllers/portfolioController')
+
+const router = require('express').Router()
+
+router.get('/:id', portfolioController.getOnePortfolio)
+router.get('/allPortfolios', portfolioController.getAllPortfolios)
+router.get('/getPortfolioPhotos/:id', portfolioController.getPortfolioPhotos)
+router.post('/addPortfolio', portfolioController.addPortfolio)
+router.put('/:id', portfolioController.updatePortfolio)
+router.delete('/:id', portfolioController.deletePortfolio)
+
+module.exports = router
