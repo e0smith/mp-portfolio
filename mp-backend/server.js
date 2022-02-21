056cd7a6c3c39e7ce8f9147904d5f091f4468e89
Edward Smith Silvia <esmithsilvia@gmail.com>
2022-02-20 21:51:13 -0500

    app.js changed to server.js

---
+++ mp-backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+// middleware
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
+// routers
+const router = require('./routes/portfolioRouter.js')
+app.use('/api/portfolios', router)
+
+
+//static Images Folder
+app.use('/Images', express.static('./Images'))
+
+
+//port
+const PORT = process.env.PORT || 8080
+
+//server
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+})
