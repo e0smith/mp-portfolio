df5d3d2cf17264f81611cb63d8bfb24b2ef5dfa8
Edward Smith Silvia <esmithsilvia@gmail.com>
2022-03-09 01:03:44 -0500

    redux store created

--- mp-portfolio/src/index.js
+++ mp-portfolio/src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore, compose, combineReducers } from 'redux'
+import rootReducer from './actions/reducers/rootReducer';
+
+const store = createStore(rootReducer, compose(
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ 
+  && window.__REDUX_DEVTOOLS_EXTENSION__())
+  )
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +25,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
