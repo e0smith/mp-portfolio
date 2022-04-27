35d6d82d5ccda183d5ddac32eb603ebef93ab3b4
Edward Smith Silvia <esmithsilvia@gmail.com>
2022-04-27 15:37:38 -0400

    testing with props within portfolioCard

--- mp-portfolio/src/Components/Cards/portfolioCard.js
+++ mp-portfolio/src/Components/Cards/portfolioCard.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { GPTitleBox, GPTitle, PortfolioBox, Hover} from "../Container.style";
-export const PortfolioCard = (port) =>{
-    console.log(port.title)
+import { MiniPhotoCard } from "./miniPhotoCard";
+import { useSelector } from "react-redux";
+import PhotoCard from "./photoCard";
+export function PortfolioCard(port, photo){
+
+    const { title, id } = port
+
+    // const photoArray = useSelector(state => state.photos);
+
+    // console.log(photoArray)
+    // let photos = photo.map(photo => {
+    //     (photo.portfolio_id == id ?  MiniPhotoCard(photo) : x = photo)})
+
+    console.log("photo = ", photo)
     return(
         <PortfolioBox>
             <Hover>
                 <GPTitleBox>
                     <GPTitle>
-                        {port.title}
+                        {title}
                     </GPTitle>
                 </GPTitleBox>
             </Hover>
