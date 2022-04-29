cddf6dd7c92ff6fb9e997d559766234a22bb961a
Edward Smith Silvia <esmithsilvia@gmail.com>
2022-04-29 15:43:11 -0400

    temporary fix to call photos from database

--- mp-portfolio/src/Components/Cards/miniPhotoCard.js
+++ mp-portfolio/src/Components/Cards/miniPhotoCard.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { PhotoCardStyle } from "../Container.style";
+// import { Card } from 'react-bootstrap';
 
 export const MiniPhotoCard = (photo) => {
-    const {image, id} = photo
+    const {image, id} = photo.photo
+    // console.log("IMAGE :", image)
+    // const parsed = image.split("/")
+    // console.log("PARSED: ", parsed)
+
+    let url = `http://localhost:8080/${image}`
+    
     return(
         <PhotoCardStyle>
-            <img src={image} alt="..."/>
+            <img src={url}/>            
         </PhotoCardStyle>
     )
 } 
