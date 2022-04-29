c08fb6339750130424cc80f4b53c529cd0a25243
Edward Smith Silvia <esmithsilvia@gmail.com>
2022-04-29 15:41:06 -0400

    mini photo showing up within gallery portfolios

--- mp-portfolio/src/Components/Cards/portfolioCard.js
+++ mp-portfolio/src/Components/Cards/portfolioCard.js
@@ -3,9 +3,7 @@ import { GPTitleBox, GPTitle, PortfolioBox, Hover} from "../Container.style";
 import { MiniPhotoCard } from "./miniPhotoCard";
 import { useSelector } from "react-redux";
 import PhotoCard from "./photoCard";
-export function PortfolioCard(port, photo){
-
-    const { title, id } = port
+export function PortfolioCard(photos){
 
     // const photoArray = useSelector(state => state.photos);
 
@@ -13,16 +11,41 @@ export function PortfolioCard(port, photo){
     // let photos = photo.map(photo => {
     //     (photo.portfolio_id == id ?  MiniPhotoCard(photo) : x = photo)})
 
-    console.log("photo = ", photo)
-    return(
+    // let photos = photo.photo.find(p => p.portfolio_id === port.params.id)
+
+    // let portList = port.map((p) => {return(
+    //     <PortfolioBox>
+    //         <Hover>
+    //             <GPTitleBox>
+    //                 <GPTitle>
+    //                     {p.title}
+    //                     {/* <MiniPhotoCard photo={photos}/> */}
+    //                 </GPTitle>
+    //             </GPTitleBox>
+    //         </Hover>
+    //     </PortfolioBox>
+    // )})
+
+    console.log(photos.photos)
+    const portfolioArray = useSelector(state => state.portfolios.portfolios);
+    let listOfPortfolios = portfolioArray.map((port) => {return(
         <PortfolioBox>
             <Hover>
                 <GPTitleBox>
                     <GPTitle>
-                        {title}
+                        {port.title}
                     </GPTitle>
                 </GPTitleBox>
+                {photos.photos.map((p) => {if(p.portfolio_id === port.id){
+                    return(<MiniPhotoCard photo={p} />)
+                }})}
             </Hover>
         </PortfolioBox>
+    )})
+
+    return(
+        <div>
+            {listOfPortfolios}
+        </div>
     )
 }
